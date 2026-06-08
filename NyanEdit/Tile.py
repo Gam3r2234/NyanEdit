8d7cc1329d7e3bfc75a3e7a3e9056aadda134709
Snow <171446723+Gam3r2234@users.noreply.github.com>
2026-06-08 07:53:26 -0400

    Delete NyanEdit directory
    Removed incorrectly added folder.

--- NyanEdit/Tile.py
+++
@@ -1,8 +0,0 @@
-class Tile:
-    def __init__(self, Hex, Type, image, coords, size, drawable):
-        self.hex = Hex
-        self.type = Type
-        self.image = image
-        self.coords = coords
-        self.size = size
-        self.drawable = drawable
